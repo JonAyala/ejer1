c60680cd55af784788ade56abdaaba3018750359
Jon Ayala <jon.ayalal@alumni.mondragon.edu>
2024-05-09 17:54:14 +0200

    he guardado el archivo ahora, el commit de antes no estaba completo, no pasa nada

---
+++ Script.R
@@ -0,0 +1,31 @@
+library(readxl)
+library(naniar)
+library(dplyr)
+
+#5
+datos<-read_xlsx("datos/online_retail_II.xlsx")
+str(datos)
+datos$`Customer ID`<-as.character(datos$`Customer ID`)
+
+
+#6
+miss_var_summary(datos)
+miss_case_summary(datos)
+
+#vis_miss(datos, cluster = T)
+
+sum(is.na(datos))
+
+
+#7
+n_distinct(datos$Invoice)
+
+
+#8
+precio_medio_producto<- datos %>% 
+  group_by(Invoice) %>% 
+  summarize(Precio_medio = mean(Price))
+
+
+
+
